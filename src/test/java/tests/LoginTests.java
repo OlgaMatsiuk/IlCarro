f0e18c68a2f9dc071361df668da004bd4e2dfe03
OlgaMatsiuk <matsiuk2015@gmail.com>
2023-06-21 16:18:02 +0300

    QA19_CW8
    Login negative test

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -6,12 +6,12 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTests extends TestBase{
+public class LoginTests extends TestBase {
 
     @BeforeMethod
 
-    public void precondition(){
-        if(app.getUser().isLogged()) {
+    public void precondition() {
+        if (app.getUser().isLogged()) {
             app.getUser().logout();
         }
     }
@@ -19,31 +19,27 @@ public class LoginTests extends TestBase{
 
     @Test
     public void loginPositive() {
-        String email = "matsiuk2015@gmail.com", password="Aravaavara0#";
+        String email = "matsiuk2015@gmail.com", password = "Aravaavara0#";
 
         app.getUser().openLoginForm();
         app.getUser().fillLoginForm(email, password);
         app.getUser().submitLogin();
         Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//h1[normalize-space()='Logged in']")));
         app.getUser().confirmMessage();
-       // app.getUser().pause(2000);
+        // app.getUser().pause(2000);
 
     }
+
     @Test
     public void loginPositiveUser() {
         //String email = "matsiuk2015@gmail.com", password="Aravaavara0#";
 
-       User user = new User().withEmail("matsiuk2015@gmail.com").withPassword("Aravaavara0#");
-
-
-
-//        user.setName("John");
-//        user.setLastName("Silver");
+        User user = new User().withEmail("matsiuk2015@gmail.com").withPassword("Aravaavara0#");
 
 
         app.getUser().openLoginForm();
-       // app.getUser().fillLoginForm(email, password);
-       // app.getUser().fillLoginForm(user.getEmail(), user.getPassword());
+        // app.getUser().fillLoginForm(email, password);
+        // app.getUser().fillLoginForm(user.getEmail(), user.getPassword());
         app.getUser().fillLoginForm(user);
         app.getUser().submitLogin();
         Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//h1[normalize-space()='Logged in']")));
@@ -52,4 +48,27 @@ public class LoginTests extends TestBase{
 
     }
 
+    @Test
+    public void loginNegativeUser() {
+        //String email = "matsiuk2015@gmail.com", password="Aravaavara0#";
+
+        User user = new User().withEmail("matsiuk+2015gmailcom").withPassword("Aravaavara0");
+
+
+        app.getUser().openLoginForm();
+        // app.getUser().fillLoginForm(email, password);
+        // app.getUser().fillLoginForm(user.getEmail(), user.getPassword());
+        app.getUser().fillLoginForm(user);
+        app.getUser().submitLogin();
+        if (app.getUser().isElementPresent(By.xpath("//button[@disabled]"))) {
+            System.out.println("Email or password are not valid");
+        } else {
+            Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//h1[normalize-space()='Login failed']")));
+            app.getUser().confirmMessage();
+            // app.getUser().pause(2000);
+
+        }
+    }
 }
+
+
